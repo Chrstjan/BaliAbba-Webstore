4bcb952c107d7114bed6ff5a4f74169d92ebdf7f
Chrstjan <ps5christian@hotmail.com>
2024-02-12 13:41:45 +0100

    #7 Created getProductData Function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,18 @@
+getCategoryData();
+getProductData();
+
 function getCategoryData() {
     fetch('https://dummyjson.com/products/categories')
     .then(response => response.json())
     .then(json => console.log(json))
 
     //recivedCategoryData(json);
+}
+
+function getProductData() {
+    fetch('https://dummyjson.com/products')
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+    //recivedProductData(json);
 }
