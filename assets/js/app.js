a9f5a8b6c0b255a6c7837e623b8322eb5e163838
Chrstjan <ps5christian@hotmail.com>
2024-02-13 12:32:00 +0100

    #9 Error Catching

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,9 +23,9 @@ function getProductData() {
         if (!response.ok) {
             throw new Error('Network response was not ok');
         } 
-        return response.json()
+        return response.json() 
      })
-     
+
     .then(json => {
         recivedProductData(json);
     })
