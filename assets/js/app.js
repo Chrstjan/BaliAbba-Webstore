d43cd55be4ea587165a5a603448fb4707bfe577f
Chrstjan <ps5christian@hotmail.com>
2024-02-13 12:09:53 +0100

    #9 In Progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,11 +12,24 @@ function getCategoryData() {
 function getProductData() {
     fetch('https://dummyjson.com/products?limit=0')
     .then(response => response.json())
-    .then(json => console.log(json))
-
-    //recivedProductData(json);
+    .then(json => {
+        recivedProductData(json);
+    })
 }
 
 function recivedCategoryData(categoryData) {
     console.log(categoryData);
+}
+
+function recivedProductData(productData) {
+    const allProducts = productData.products;
+    console.log(allProducts);
+
+    //Random featured products
+    const featuredProductsArray = [];
+    featuredProductsArray.push(
+     allProducts[Math.floor(Math.random() * allProducts.length)],
+     allProducts[Math.floor(Math.random() * allProducts.length)], 
+     allProducts[Math.floor(Math.random() * allProducts.length)]
+     );
 }
